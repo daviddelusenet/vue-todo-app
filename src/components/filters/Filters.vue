0399d877d08437a8bdfcdb3fa953f4d47c7d42ae
David de Lusenet <>
2021-04-16 08:17:35 +0200

    feat: add some styling to Filters component

--- src/components/filters/Filters.vue
+++ src/components/filters/Filters.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
+  <div class="filters">
     <p>Sort by:</p>
-    <select @change="onSort($event)" :value="sortBy">
-      <option
-        v-for="option in options"
-        :key="option.value"
-        :value="option.value"
-      >
-        {{ option.text }}
-      </option>
-    </select>
+    <div class="select-wrapper">
+      <select @change="onSort($event)" :value="sortBy">
+        <option
+          v-for="option in options"
+          :key="option.value"
+          :value="option.value"
+        >
+          {{ option.text }}
+        </option>
+      </select>
+      <font-awesome-icon :icon="['fas', 'chevron-down']" />
+    </div>
   </div>
 </template>
 
@@ -54,9 +57,41 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-div {
+.filters {
   display: flex;
   align-items: center;
   padding: 0 20px 20px;
+}
+
+p {
+  margin: 0 8px 0 0;
+}
+
+.select-wrapper {
+  position: relative;
+
+  select {
+    border: 1px solid var(--secondary-color);
+    height: 32px;
+    outline: none;
+    padding: 0 32px 0 8px;
+
+    &:focus,
+    &:active {
+      border-color: var(--highlight-color);
+
+      & + svg {
+        color: var(--primary-button-background-color);
+      }
+    }
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    transform: translate3d(0, -50%, 0);
+    right: 8px;
+    color: var(--secondary-color);
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCheck, faPlus, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faChevronDown,
+  faPlus,
+  faTrashAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /**
  * Add icons to the library to use the advantage of tree shaking
  * @see https://github.com/FortAwesome/vue-fontawesome#why-use-the-concept-of-a-library
+ * Availabe icons
+ * @see https://fontawesome.com/icons?d=gallery&p=2&s=solid&m=free
  */
-library.add(faCheck, faPlus, faTrashAlt);
+library.add(faCheck, faChevronDown, faPlus, faTrashAlt);
 
 /**
  * Add a global font-awesome-icon component add mount the application
